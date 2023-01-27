0d122e3a0d6ecc75ea251fa6636aaccda9cf1951
Nikhil <nikhilsagar592000@gmail.com>
2023-01-28 01:16:22 +0530

    feat: buttons setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ function App() {
       <p className="text-primary-500">Primary text</p>
       <p className="text-secondary-500">Secondary text</p>
       <p className="text-tertiary-500">Tertiary text</p>
+      <div className="flex gap-4 p-4 items-center ">
+        <button className="btn-primary-small">Primary Button</button>
+        <button className="btn-primary">Primary Button</button>
+        <button className="btn-primary-large">Primary Button</button>
+      </div>
+      <div className="flex gap-4 p-4 items-center ">
+        <button className="btn-secondary-small">Secondary Button</button>
+        <button className="btn-secondary">Secondary Button</button>
+        <button className="btn-secondary-large">Secondary Button</button>
+      </div>
+      <div className="flex gap-4 p-4 items-center ">
+        <button className="btn-tertiary-small">Tertiary Button</button>
+        <button className="btn-tertiary">Tertiary Button</button>
+        <button className="btn-tertiary-large">Tertiary Button</button>
+      </div>
     </div>
   );
 }
